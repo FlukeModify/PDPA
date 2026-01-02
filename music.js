9f556976c906eb54e44c6d17bfbebe9f25a85401
FlukeModify <flukessd1@gmail.com>
2026-01-02 15:28:40 +0700

    Delete music.js

--- music.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-  const bgm = document.getElementById('bgm');
-  bgm.play().catch(() => {
-    // ถ้าเบราว์เซอร์บล็อค autoplay
-    document.body.addEventListener('click', () => {
-      bgm.play();
-    }, { once: true });
-  });
-});
